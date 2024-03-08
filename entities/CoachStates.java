ce6fca3a083dac938209eee6c53cb07b43c26dae
RafaelSGil <rafaelgil131@gmail.com>
2024-03-08 12:42:06 +0000

    Implemented the initial code structure

---
+++ entities/CoachStates.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class CoachStates {
+    public static final int WATFORREFEREECOMMAND = 0;
+    public static final int ASSEMBLETEAM = 1;
+    public static final int WATCHTRIAL = 2;
+
+    private CoachStates(){}
+}
